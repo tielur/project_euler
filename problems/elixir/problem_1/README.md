8a540466fb5f89668fb7bde1bad002f5d498c228
Tyler Clemens <tyler.r.clemens@gmail.com>
2016-09-28 16:08:08 -0700

    problem 1

---
+++ problems/elixir/problem_1/README.md
@@ -0,0 +1,24 @@
+# Problem1
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `problem_1` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:problem_1, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `problem_1` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:problem_1]]
+    end
+    ```
+
---
+++ problems/elixir/problem_1/lib/problem_1.ex
@@ -0,0 +1,24 @@
+defmodule Problem1 do
+  @moduledoc """
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+  """
+
+  @doc ~S"""
+  Calculates the sum of all the multiples of 3 or 5 below `limit`.
+
+   ## Examples
+
+      iex> Problem1.solve(10)
+      23
+
+      iex> Problem1.solve
+      233168
+  """
+  def solve(limit \\1000) do
+    (1..limit-1)
+    |> Stream.filter(fn(num) -> 0 == rem(num, 3) or 0 == rem(num, 5) end)
+    |> Enum.sum
+  end
+end
